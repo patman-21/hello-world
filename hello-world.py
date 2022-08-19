a7b1f07bfaec073ba8935fbb609021e7819e3b01
Pat-21 <lovellljoy3@gmail.com>
2022-08-19 20:16:08 +0100

    my first changes: - math operations, variables, the input function

---
+++ hello-world.py
@@ -0,0 +1,26 @@
+# print("hello world")
+
+# print(5+4)
+# print(15-3 )
+# print(9/3)
+# print(4*3) 
+# print(5 +  3 / 2)
+
+# print(2**4)
+# print(5//3)
+# print(5%3)
+
+# this is my comment
+3+2 # end of line comment 
+
+# red_bucket = "Pat"wow
+# red_bucket = input("what do you want to put in the bucket? ")
+
+# print(red_bucket)
+
+persons_age = int(input("how old are you? "))
+retirement_age = 66 
+can_collect_pension = persons_age >= retirement_age
+
+print(can_collect_pension)
+
